a03818e43ba32e336abc694bcc35afcc73b02091
asagishi <asagishi.ad@gmail.com>
2016-10-14 20:25:57 +0700

    Update

--- safelink-3-multi.js
+++ safelink-3-multi.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="review-anime-koe-no-katachi.html"}
 if(x=="2"){xxx="review-anime-kyoukai-no-kanata-mirai-hen.html"}
 if(x=="3"){xxx="top-10-anime-magic.html"}
 if(x=="4"){xxx="top-10-anime-school.html"}
-if(x=="5"){xxx="top-10-anime-comedy.html"}a_to_ve[i].href="http://luja.ga/2016/10/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="top-10-anime-comedy.html"}a_to_ve[i].href="http://www.luja.ga/2016/10/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
